e6be82fb6ce2be84b9597aeb3e95781211dbc3b3
unknown <jazminserna1313@gmail.com>
2025-10-21 15:28:21 -0600

    modified:   modular.sql

--- modular.sql
+++ modular.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-10-2025 a las 01:12:46
+-- Tiempo de generación: 21-10-2025 a las 23:26:39
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -37,6 +37,19 @@ CREATE TABLE `ejercicio` (
   `nivel` decimal(5,0) NOT NULL,
   `clasificacion` tinyint(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `ejercicio`
+--
+
+INSERT INTO `ejercicio` (`id_ejercicio`, `nom_ejercicio`, `descripcion_ejer`, `ejemplo_ejer`, `puntuacion`, `n_puntuacion`, `nivel`, `clasificacion`) VALUES
+(1, 'Balanceo de piernas de adelant', 'Ejercicio / Cadera, Pierna\r\n\r\n\r\n1.Párese erguido junto a una pared o un objeto firme para mantener el equilibrio.\r\n\r\n2.Transfiere tu peso a una pierna y levanta ligeramente la pierna opuesta del suelo.\r\n\r\n3.Balancee la pierna levantada hacia adelante, manteniendo la rodilla recta o ligeramente doblada.\r\n\r\n4.Permita que la pierna se balancee hacia atrás, creando un arco suave de adelante hacia atrás.\r\n\r\n5.Realice de 10 a 15 movimientos por pierna, aumentando el rango de movimiento gradualmente co', 'balanceo_de_piernas_de_adelante_hacia_atras.gif', 0, 0, 1, 1),
+(2, 'Navy Seal Burpee', 'Ejercicio / Cardio, Cuerpo completo\r\n\r\nComience desde una posición de pie con los pies separados al ancho de los hombros.\r\n\r\nPonte en cuclillas, coloca las manos en el suelo y patea los pies hacia atrás en una posición de flexión.\r\n\r\nRealice una flexión, luego conduzca la rodilla derecha hacia el codo derecho.\r\n\r\nRealice una segunda flexión, luego conduzca la rodilla izquierda hacia el codo izquierdo.\r\n\r\nRealice una tercera flexión, luego salte los pies hacia adelante para volver a la posición d', 'burpee_de_los_navy _seal.gif', 0, 0, 3, 2),
+(3, 'Estocada de péndulo', 'Ejercicio / Cadera, Pierna\r\n\r\n\r\nPosición inicial: Párese derecho con los pies separados al ancho de las caderas, las manos entrelazadas en las caderas o frente al pecho para mantener el equilibrio.\r\n\r\nEstocada hacia adelante: Da un paso adelante con una pierna, bajando las caderas hasta que el muslo delantero esté casi paralelo al piso, manteniendo el torso erguido.\r\n\r\nTransición: Sin detenerse, empuje el pie delantero y balancee la misma pierna hacia atrás en una estocada inversa.\r\n\r\nEstocada i', 'estocada_de_pendulo.gif', 0, 0, 2, 1),
+(4, 'Peso muerto con mancuernas', 'Ejercicio / erector de la columna, cuerpo completo, cadera, pierna\r\n\r\nArreglo:\r\nPárate con los pies separados al ancho de las caderas. Coloque una mancuerna a cada lado de sus pies.\r\nLas mancuernas deben colocarse paralelas a sus pies, de modo que las manijas estén perpendiculares a su cuerpo.\r\nPosición:\r\nDobla las caderas y las rodillas para bajar.\r\nMantenga la espalda recta, el pecho hacia arriba y los hombros hacia atrás.\r\nSostenga las pesas con un agarre firme, con las palmas hacia su cuerpo', 'peso_muerto_con_macuernas.gif', 0, 0, 3, 3),
+(5, 'Sentadilla Pie con peso corpor', 'Ejercicio / Cadera, Pierna\r\n\r\nPárese con los pies más anchos que el ancho de los hombros y gire los dedos de los pies en un ángulo de aproximadamente 45 grados.\r\n\r\nMantenga el pecho alto y comprometa su núcleo para sostener su columna vertebral.\r\n\r\nEmpuje las rodillas en línea con los dedos de los pies mientras comienza a bajar a la sentadilla.\r\n\r\nDobla las rodillas y baja las caderas hacia abajo hasta que los muslos estén paralelos al suelo o ligeramente más bajos.\r\n\r\nHaga una breve pausa en la', 'sentadilla_a_pie_con_peso_corporal.gif', 0, 0, 1, 1),
+(6, 'Salto de longitud Burpee', 'Ejercicio / Cardio, Cuerpo completo\r\n\r\nPosición inicial:\r\n\r\nPárate erguido con los pies separados al ancho de los hombros.\r\n\r\nMantén tu núcleo apoyado y los brazos relajados a los lados.\r\n\r\nFase de burpee:\r\n\r\nPonte en cuclillas y coloca las manos en el suelo frente a ti.\r\n\r\nSalta o retrocede los pies a una posición de tabla alta.\r\n\r\nRealiza una flexión (opcional pero común en esta variación).\r\n\r\nVolver a la sentadilla:\r\n\r\nSalta o da un paso adelante con los pies para que caigan justo fuera de tu', 'salto_de_longitud_con_burpee.gif', 0, 0, 4, 2),
+(7, 'Sentadilla con copa elevada co', 'Ejercicio / Cadera, Pierna\r\n\r\nPárese con los talones elevados sobre discos de pesas, cuñas en cuclillas o una tabla inclinada, y los dedos de los pies apoyados en el piso.\r\n\r\nSostenga una mancuerna o pesa rusa verticalmente a la altura del pecho con ambas manos (agarre de copa).\r\n\r\nMantenga el pecho alto y el núcleo contraído mientras comienza a descender.\r\n\r\nPóngase en cuclillas lentamente, permitiendo que sus rodillas se desplacen hacia adelante sobre los dedos de los pies mientras mantiene el', 'sentadilla_con_copa_con_elevaciones_del_talon.gif\r\n', 0, 0, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -103,10 +116,17 @@ CREATE TABLE `rutina` (
   `nom_rutina` varchar(100) NOT NULL,
   `color` varchar(30) NOT NULL,
   `icono` varchar(30) NOT NULL,
-  `nivel` decimal(10,0) NOT NULL DEFAULT 0,
-  `puntuacion` decimal(10,0) NOT NULL DEFAULT 0,
-  `n_puntuacion` int(5) NOT NULL DEFAULT 0
+  `nivel` decimal(5,0) NOT NULL,
+  `puntuacion` decimal(5,0) NOT NULL,
+  `n_puntuacion` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `rutina`
+--
+
+INSERT INTO `rutina` (`id_rutina`, `nom_rutina`, `color`, `icono`, `nivel`, `puntuacion`, `n_puntuacion`) VALUES
+(1, 'nombre', '#DC3545', 'directions_run', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -151,8 +171,8 @@ ALTER TABLE `musculo`
 -- Indices de la tabla `rel_ejer_rutina_musculo`
 --
 ALTER TABLE `rel_ejer_rutina_musculo`
+  ADD KEY `id_ejercicio` (`id_ejercicio`,`id_rutina`,`id_musculo`),
   ADD KEY `id_rutina` (`id_rutina`),
-  ADD KEY `id_ejercicio` (`id_ejercicio`,`id_musculo`),
   ADD KEY `id_musculo` (`id_musculo`);
 
 --
@@ -175,7 +195,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `ejercicio`
 --
 ALTER TABLE `ejercicio`
-  MODIFY `id_ejercicio` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ejercicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `herramienta`
@@ -193,7 +213,7 @@ ALTER TABLE `musculo`
 -- AUTO_INCREMENT de la tabla `rutina`
 --
 ALTER TABLE `rutina`
-  MODIFY `id_rutina` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_rutina` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -209,9 +229,9 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `rel_ejer_rutina_musculo`
 --
 ALTER TABLE `rel_ejer_rutina_musculo`
-  ADD CONSTRAINT `rel_ejer_rutina_musculo_ibfk_3` FOREIGN KEY (`id_ejercicio`) REFERENCES `ejercicio` (`id_ejercicio`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `rel_ejer_rutina_musculo_ibfk_4` FOREIGN KEY (`id_musculo`) REFERENCES `musculo` (`id_musculo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `rel_ejer_rutina_musculo_ibfk_5` FOREIGN KEY (`id_rutina`) REFERENCES `rutina` (`id_rutina`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `rel_ejer_rutina_musculo_ibfk_1` FOREIGN KEY (`id_rutina`) REFERENCES `rutina` (`id_rutina`),
+  ADD CONSTRAINT `rel_ejer_rutina_musculo_ibfk_2` FOREIGN KEY (`id_musculo`) REFERENCES `musculo` (`id_musculo`),
+  ADD CONSTRAINT `rel_ejer_rutina_musculo_ibfk_3` FOREIGN KEY (`id_ejercicio`) REFERENCES `ejercicio` (`id_ejercicio`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
